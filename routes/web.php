456d0728e72f71a15e2a0163245d02b45b58b564
Nguyen Dinh Toi <dinhtoi1606th@gmail.com>
2018-06-01 11:06:03 +0700

    Sửa router

--- routes/web.php
+++ routes/web.php
@@ -15,91 +15,91 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/facebook/login', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
-{
-    // Send an array of permissions to request
-    $login_url = $fb->getLoginUrl(['email']);
+// Route::get('/facebook/login', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
+// {
+//     // Send an array of permissions to request
+//     $login_url = $fb->getLoginUrl(['email']);
 
-    // Obviously you'd do this in blade :)
-    echo '<a href="' . $login_url . '">Login with Facebook</a>';
-});
+//     // Obviously you'd do this in blade :)
+//     echo '<a href="' . $login_url . '">Login with Facebook</a>';
+// });
 
-// Endpoint that is redirected to after an authentication attempt
-Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
-{
-    // Obtain an access token.
-    try {
-        $token = $fb->getAccessTokenFromRedirect();
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
+// // Endpoint that is redirected to after an authentication attempt
+// Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb)
+// {
+//     // Obtain an access token.
+//     try {
+//         $token = $fb->getAccessTokenFromRedirect();
+//     } catch (Facebook\Exceptions\FacebookSDKException $e) {
+//         dd($e->getMessage());
+//     }
 
-    // Access token will be null if the user denied the request
-    // or if someone just hit this URL outside of the OAuth flow.
-    if (! $token) {
-        // Get the redirect helper
-        $helper = $fb->getRedirectLoginHelper();
+//     // Access token will be null if the user denied the request
+//     // or if someone just hit this URL outside of the OAuth flow.
+//     if (! $token) {
+//         // Get the redirect helper
+//         $helper = $fb->getRedirectLoginHelper();
 
-        if (! $helper->getError()) {
-            abort(403, 'Unauthorized action.');
-        }
+//         if (! $helper->getError()) {
+//             abort(403, 'Unauthorized action.');
+//         }
 
-        // User denied the request
-        dd(
-            $helper->getError(),
-            $helper->getErrorCode(),
-            $helper->getErrorReason(),
-            $helper->getErrorDescription()
-        );
-    }
+//         // User denied the request
+//         dd(
+//             $helper->getError(),
+//             $helper->getErrorCode(),
+//             $helper->getErrorReason(),
+//             $helper->getErrorDescription()
+//         );
+//     }
 
-    if (! $token->isLongLived()) {
-        // OAuth 2.0 client handler
-        $oauth_client = $fb->getOAuth2Client();
+//     if (! $token->isLongLived()) {
+//         // OAuth 2.0 client handler
+//         $oauth_client = $fb->getOAuth2Client();
 
-        // Extend the access token.
-        try {
-            $token = $oauth_client->getLongLivedAccessToken($token);
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
-    }
+//         // Extend the access token.
+//         try {
+//             $token = $oauth_client->getLongLivedAccessToken($token);
+//         } catch (Facebook\Exceptions\FacebookSDKException $e) {
+//             dd($e->getMessage());
+//         }
+//     }
 
-    $fb->setDefaultAccessToken($token);
+//     $fb->setDefaultAccessToken($token);
 
-    // Save for later
-    Session::put('fb_user_access_token', (string) $token);
+//     // Save for later
+//     Session::put('fb_user_access_token', (string) $token);
 
-    // Get basic info on the user from Facebook.
-    try {
-        $response = $fb->get('/me?fields=id,name,friends,link,email,gender,picture,first_name,last_name,cover,location,accounts');
+//     // Get basic info on the user from Facebook.
+//     try {
+//         $response = $fb->get('/me?fields=id,name,friends,link,email,gender,picture,first_name,last_name,cover,location,accounts');
  
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
+//     } catch (Facebook\Exceptions\FacebookSDKException $e) {
+//         dd($e->getMessage());
+//     }
 
-    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-    $facebook_user = $response->getGraphUser();
-    $id = $facebook_user['accounts'][0]['id'];
-    $token = $facebook_user['accounts'][0]['access_token'];
-    $response1 = $fb->get('/1749794068443468/feed');
-    $facebook_user1 = $response1->getGraphUser();
+//     // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
+//     $facebook_user = $response->getGraphUser();
+//     $id = $facebook_user['accounts'][0]['id'];
+//     $token = $facebook_user['accounts'][0]['access_token'];
+//     $response1 = $fb->get('/1749794068443468/feed');
+//     $facebook_user1 = $response1->getGraphUser();
     
-   print_r($facebook_user);
-   die();
+//    print_r($facebook_user);
+//    die();
 
-    // Create the user if it does not exist or update the existing entry.
-    // This will only work if you've added the SyncableGraphNodeTrait to your User model.
+//     // Create the user if it does not exist or update the existing entry.
+//     // This will only work if you've added the SyncableGraphNodeTrait to your User model.
   
 
-    // Log the user into Laravel
+//     // Log the user into Laravel
    
 
-    return redirect('/')->with('message', 'Successfully logged in with Facebook');
-});
-
-// Route::get('auth/{provider}', 'SocialAuthController@redirectToProvider');
-// Route::get('auth/{provider}/callback', 'SocialAuthController@handleProviderCallback');
-// Route::get('/login', function () {
-//     return view('login');
+//     return redirect('/')->with('message', 'Successfully logged in with Facebook');
 // });
+
+Route::get('auth/{provider}', 'SocialAuthController@redirectToProvider');
+Route::get('auth/{provider}/callback', 'SocialAuthController@handleProviderCallback');
+Route::get('/login', function () {
+    return view('login');
+});
